b12178368e2cedc723cc7e8510808dfd634042e4
Aliya Shakirova <aliya.sulimova@gmail.com>
2024-03-23 22:34:58 +0200

    removed not relevant comments

--- Program.cs
+++ Program.cs
@@ -4,8 +4,5 @@ Console.WriteLine("Enter username:");
 string username = Console.ReadLine();
 
 Console.WriteLine($"Hello {username}!");
-<<<<<<< HEAD
 // test test
-=======
-// test 
->>>>>>> 5e64afa49c9b5e7d382233f22ed94232909978c7
+
